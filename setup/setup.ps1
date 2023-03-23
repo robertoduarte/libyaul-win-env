42f17ccb78b33116a9a5919b85bcecc0474800d5
Danny <robertodanielalvesduarte@gmail.com>
2023-03-23 16:54:29 +0000

    Removed redundant code and fixed temporary folder management on setup script

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -1,73 +1,11 @@
-
-
-
 $tmpPath = "$pwd\temp"
-Remove-Item -Recurse -Force -Path $tmpPath
-if (-Not $(Test-Path $tmpPath)) {
-    New-Item $tmpPath -ItemType Directory
+
+if ($(Test-Path $tmpPath)) {
+    Remove-Item -Recurse -Force -Path $tmpPath
 }
 
+New-Item $tmpPath -ItemType Directory
 
-function Get-Folder {
-    param ()
-    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-
-    [System.Reflection.BindingFlags] $DialogFlags = [System.Reflection.BindingFlags]::Instance + [System.Reflection.BindingFlags]::Public + [System.Reflection.BindingFlags]::NonPublic;
-    [string] $DialogFoldersFilter = "Folders|\n";
-    [System.UInt32] $AdviseType = 0;
-    [System.Reflection.Assembly] $AssemblyOfWinForm = [System.Windows.Forms.FileDialog].Assembly
-    [System.Type] $FileDialogType = $AssemblyOfWinForm.GetType("System.Windows.Forms.FileDialogNative+IFileDialog")
-    [System.Reflection.MethodInfo] $SystemAdviseMethodInfo = $FileDialogType.GetMethod("Advise")
-    [System.Reflection.MethodInfo] $SystemCeateVistaDialogMethodInfo = [System.Windows.Forms.OpenFileDialog].GetMethod("CreateVistaDialog", $DialogFlags)
-    [System.Reflection.MethodInfo] $SystemGetOptionsMethodInfo = [System.Windows.Forms.FileDialog].GetMethod("GetOptions", $DialogFlags)
-    [System.Reflection.MethodInfo] $SystemOnBeforeVistaDialogMethodInfo = [System.Windows.Forms.OpenFileDialog].GetMethod("OnBeforeVistaDialog", $DialogFlags)
-    [System.Type] $SystemOsPickFolders = $AssemblyOfWinForm.GetType("System.Windows.Forms.FileDialogNative+FOS")
-    [System.UInt32] $SystemOsPickFoldersBitFlag = [System.UInt32]$SystemOsPickFolders.GetField("FOS_PICKFOLDERS").GetValue($null)
-    [System.Reflection.MethodInfo] $SystemSetOptionsMethodInfo = $FileDialogType.GetMethod("SetOptions", $DialogFlags)
-    [System.Reflection.MethodInfo] $SystemShowMethodInfo = $FileDialogType.GetMethod("Show")
-    [System.Reflection.MethodInfo] $SystemUnAdviseMethodInfo = $FileDialogType.GetMethod("Unadvise")
-    [System.Type] $VistaDialogEvents = $AssemblyOfWinForm.GetType("System.Windows.Forms.FileDialog+VistaDialogEvents")
-    [System.Object[]] $parameters = , [System.Windows.Forms.FileDialog]
-    [System.Reflection.ConstructorInfo] $VistaDialogEventsConstructorInfo = $VistaDialogEvents.GetConstructor($DialogFlags, $null, $parameters, $null)
-
-    [System.Windows.Forms.OpenFileDialog] $openFileDialog = New-Object -TypeName System.Windows.Forms.OpenFileDialog
-    $openFileDialog.AddExtension = $false
-    $openFileDialog.CheckFileExists = $false
-    $openFileDialog.DereferenceLinks = $true
-    $openFileDialog.Filter = $DialogFoldersFilter
-    $openFileDialog.InitialDirectory = ""
-    $openFileDialog.Multiselect = $false
-    $openFileDialog.Title = "Select a location where to install yaul"
-
-    [System.Object[]] $empty = @()
-    [System.Object] $fileDialog = $SystemCeateVistaDialogMethodInfo.Invoke($openFileDialog, $empty)
-    [System.Object[]] $fileDialogParam = , $fileDialog
-    $SystemOnBeforeVistaDialogMethodInfo.Invoke($openFileDialog, $fileDialogParam) | Out-Null
-    [System.UInt32] $flag = [System.UInt32]$SystemGetOptionsMethodInfo.Invoke($openFileDialog, $empty) + $SystemOsPickFoldersBitFlag;
-    [System.Object[]] $flags = , $flag
-    $SystemSetOptionsMethodInfo.Invoke($fileDialog, $flags) | Out-Null
-    [System.Object[]] $advParam = , $openFileDialog
-    [System.Object[]] $adviseParametersWithOutputConnectionToken = $VistaDialogEventsConstructorInfo.Invoke($advParam), $AdviseType
-    $SystemAdviseMethodInfo.Invoke($fileDialog, $adviseParametersWithOutputConnectionToken) | Out-Null
-
-    try {
-        [System.Object[]] $handle = , [System.IntPtr].Zero
-        [int] $dialogResult = [int]$SystemShowMethodInfo.Invoke($fileDialog, $handle);
-
-        if ($dialogResult -eq 0) {
-            $result = $openFileDialog.FileName.Trim()
-        }
-        else {
-            exit
-        }
-    }
-    finally {
-        [System.Object[]] $advFinParam = , $adviseParametersWithOutputConnectionToken[1]
-        $SystemUnAdviseMethodInfo.Invoke($fileDialog, $advFinParam) | Out-Null
-    }
-
-    return $result
-}
 
 Add-Type -AssemblyName System.Windows.Forms
 
